19318e991e22cf40d6d6c533fd8ee07477ce2c3e
Ferhan Abaci <ferhanabacii@gmail.com>
2022-03-08 22:25:01 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace sinif_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Söz dizimi
+            //class SinifAdi
+            //{
+            //  [Erişim Belirleyici] [Veri Tipi] ÖzellikAdi;
+            //  [Erişim Belirleyici] [ Geri Dönüş Tipi] MetotAdi([Parametre Listesi])
+            //  {
+            //     Metot Komutları
+            //  }
+            //}
+        }
+    }
+}
